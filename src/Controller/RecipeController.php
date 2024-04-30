e30f9b1871c1b6d4f1dbdfef9600131d3ada32bc
arnaudb78 <arnaudb92230@gmail.com>
2024-04-30 18:20:05 +0200

    WIP: connect db with dbvisualizer

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +11,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/recettes', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $repository): Response
     {
+        $recipes = $repository->findAll();
+        dd($recipes);
         return $this->render('recipe/index.html.twig');
     }
 
